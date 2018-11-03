aa4a3d0b56c979fef53c3e21f1e151dd8dc6627e
Jonathan <jonathan@jonathanj.eu.org>
2018-11-03 12:28:55 +0100

    update

--- resources/assets/js/controllers/HomeController.js
+++ resources/assets/js/controllers/HomeController.js
@@ -8,10 +8,66 @@
     var app = angular.module('app')
 
 
-    app.controller('HomeController', ['$scope', function($scope)
+    app.controller('HomeController', ['$scope', 'Products', function($scope, Products)
     {
         // Current page
         $scope.$emit('setCurrentPage', 'Home')
+
+        // Products
+        $scope.products = []
+        loadProducts()
+        function loadProducts()
+        {
+            Products.all()
+                .then(function(products)
+                {
+                    products.forEach(function(product) { product.quantity = 0 })
+
+                    $scope.products = products
+                })
+                .catch(function()
+                {
+                    alert("Erreur lors de la récupération des produits")
+                })
+        }
+
+        // +1 quantity
+        $scope.add = function(product)
+        {
+            if (product.stock > product.quantity)
+                product.quantity++
+        }
+
+        // -1 quantity
+        $scope.remove = function(product)
+        {
+            if (product.quantity > 0)
+                product.quantity--
+        }
+
+        $scope.isEmpty = function()
+        {
+            var isEmpty = true
+
+            $scope.products.forEach(function(product)
+            {
+                isEmpty &= product.quantity == 0
+            })
+
+            return isEmpty
+        }
+
+        $scope.getTotal = function()
+        {
+            var total = 0
+
+            $scope.products.forEach(function(product)
+            {
+                total += product.quantity * product.price
+            })
+
+            return total.toFixed(2)
+        }
     }])
 
 })();
---
+++ resources/assets/js/directives/ngRightClick.js
@@ -0,0 +1,22 @@
+
+(function()
+{
+    var app = angular.module('app')
+
+    app.directive('ngRightClick', function($parse)
+    {
+        return function(scope, element, attrs)
+        {
+            var fn = $parse(attrs.ngRightClick)
+            element.bind('contextmenu', function(event)
+            {
+                scope.$apply(function()
+                {
+                    event.preventDefault()
+                    fn(scope, {$event:event})
+                })
+            })
+        }
+    })
+
+})();
