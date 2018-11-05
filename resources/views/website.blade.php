c9eb11031005c00e41eb085af635ed3b31e5da12
Jonathan <jonathan@jonathanj.eu.org>
2018-11-05 11:07:28 +0100

    changement du titre de la fenêtre

--- resources/views/website.blade.php
+++ resources/views/website.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Angular</title>
+    <title>Amicale</title>
     <base href="/">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link href="css/style.min.css" rel="stylesheet">
